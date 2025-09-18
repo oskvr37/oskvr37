bf02139fbd1c13084f86a5973b3d6e63c8e98eb4
Oskar Dudziński <oskarxpd@gmail.com>
2025-09-18 21:12:22 +0200

    deploy website

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -11,7 +11,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  site: "https://oskvr37.github.io",
   integrations: [mdx(), sitemap()],
   vite: {
     plugins: [tailwindcss()],
